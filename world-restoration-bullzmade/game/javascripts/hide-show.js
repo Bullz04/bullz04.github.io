1b0f3097d464ec690d52d7154e47955474247039
Bullz04 <62468250+Bullz04@users.noreply.github.com>
2021-12-04 21:22:37 +0700

    Delete world-restoration-bullzmade/game/javascripts directory

--- world-restoration-bullzmade/game/javascripts/hide-show.js
+++
@@ -1,7 +0,0 @@
-function hideAndShow(a) {
-    if (document.getElementById(a).style.display == "none") {
-        document.getElementById(a).style.display = ""
-    } else {
-        document.getElementById(a).style.display = "none"
-    }
-}
